72da2dbea5063e803741c405d2fb15f769464446
Matteo Camin <macamin@IUT.BX1>
2021-06-04 12:10:58 +0200

    US 3 : validé + US 9. Allonger emprunt.s. (Camin Mattéo)

---
+++ Maquette/Maquette/MyEmprunter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Maquette
+{
+    partial class EMPRUNTER
+    {
+        public override string ToString()
+        {
+            return ALBUMS.TITRE_ALBUM.Trim()+"   "+DATE_EMPRUNT.Date.ToString()+"     "+DATE_RETOUR_ATTENDUE.Date.ToString();
+        }
+    }
+}
