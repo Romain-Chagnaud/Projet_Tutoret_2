94d3ff29fb8fcaa320a47b900d67eeda60c7f59d
Léo PUYASTIER <leo.puyastier@etu.u-bordeaux.fr>
2021-06-04 14:59:42 +0200

    Admin voit le retard

--- Maquette/Maquette/Admin.Designer.cs
+++ Maquette/Maquette/Admin.Designer.cs
@@ -31,6 +31,9 @@ namespace Maquette
         {
             this.listBox1 = new System.Windows.Forms.ListBox();
             this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.button3 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // listBox1
@@ -51,11 +54,43 @@ namespace Maquette
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(694, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Créer retard";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.HorizontalScrollbar = true;
+            this.listBox2.Location = new System.Drawing.Point(298, 49);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(490, 134);
+            this.listBox2.TabIndex = 3;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(377, 12);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
             // Admin
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.listBox1);
             this.Name = "Admin";
@@ -68,5 +103,8 @@ namespace Maquette
 
         private System.Windows.Forms.ListBox listBox1;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Button button3;
     }
 }
--- Maquette/Maquette/Admin.cs
+++ Maquette/Maquette/Admin.cs
@@ -25,6 +25,7 @@ namespace Maquette
 
             listBox1.Items.Clear();
             var emprunts = (from em in musique.EMPRUNTER
+                            where em.DATE_RETOUR == null
                             select em);
 
             foreach (EMPRUNTER em in emprunts)
@@ -37,6 +38,51 @@ namespace Maquette
             }
 
 
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            EMPRUNTER em = new EMPRUNTER();
+            var abo = (from a in musique.ABONNÉS
+                       where a.LOGIN_ABONNÉ == "michou"
+                       select a).ToList();
+
+            foreach (ABONNÉS a in abo)
+            {
+                em.ABONNÉS = a;
+            }
+
+            var alb = (from al in musique.ALBUMS
+                       where al.CODE_ALBUM == 26
+                       select al);
+
+            foreach (ALBUMS a in alb)
+            {
+                em.ALBUMS = a;
+            }
+
+            em.DATE_EMPRUNT = new DateTime(2250, 1, 28);
+            em.DATE_RETOUR_ATTENDUE = new DateTime(2021, 6, 3);
+
+            musique.EMPRUNTER.Add(em);
+            musique.SaveChanges();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            listBox2.Items.Clear();
+
+            var emprunts = (from em in musique.EMPRUNTER
+                            select em).ToList();
+
+            foreach (EMPRUNTER em in emprunts)
+            {
+                DateTime date = em.DATE_RETOUR_ATTENDUE.AddDays(10);
+                if (date.CompareTo(DateTime.Now) < 0)
+                {
+                    listBox2.Items.Add(em);
+                }
+            }
         }
     }
 }
