4c2e76102e1e8aad8f3968bc8b774aba7fb39f8a
Léo PUYASTIER <leo.puyastier@etu.u-bordeaux.fr>
2021-06-04 10:29:43 +0200

    ID : michou MDP : 1234

--- Maquette/Maquette/Form1.Designer.cs
+++ Maquette/Maquette/Form1.Designer.cs
@@ -29,13 +29,106 @@ namespace Maquette
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.nom = new System.Windows.Forms.TextBox();
+            this.prenom = new System.Windows.Forms.TextBox();
+            this.login = new System.Windows.Forms.TextBox();
+            this.mdp = new System.Windows.Forms.TextBox();
+            this.log2 = new System.Windows.Forms.TextBox();
+            this.mdp2 = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // nom
+            // 
+            this.nom.Location = new System.Drawing.Point(21, 46);
+            this.nom.Name = "nom";
+            this.nom.Size = new System.Drawing.Size(100, 20);
+            this.nom.TabIndex = 0;
+            // 
+            // prenom
+            // 
+            this.prenom.Location = new System.Drawing.Point(21, 93);
+            this.prenom.Name = "prenom";
+            this.prenom.Size = new System.Drawing.Size(100, 20);
+            this.prenom.TabIndex = 1;
+            // 
+            // login
+            // 
+            this.login.Location = new System.Drawing.Point(21, 216);
+            this.login.Name = "login";
+            this.login.Size = new System.Drawing.Size(100, 20);
+            this.login.TabIndex = 2;
+            // 
+            // mdp
+            // 
+            this.mdp.Location = new System.Drawing.Point(21, 242);
+            this.mdp.Name = "mdp";
+            this.mdp.Size = new System.Drawing.Size(100, 20);
+            this.mdp.TabIndex = 3;
+            // 
+            // log2
+            // 
+            this.log2.Location = new System.Drawing.Point(496, 73);
+            this.log2.Name = "log2";
+            this.log2.Size = new System.Drawing.Size(100, 20);
+            this.log2.TabIndex = 4;
+            // 
+            // mdp2
+            // 
+            this.mdp2.Location = new System.Drawing.Point(496, 138);
+            this.mdp2.Name = "mdp2";
+            this.mdp2.Size = new System.Drawing.Size(100, 20);
+            this.mdp2.TabIndex = 5;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(477, 238);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(235, 186);
+            this.listBox1.TabIndex = 6;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(63, 311);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.mdp2);
+            this.Controls.Add(this.log2);
+            this.Controls.Add(this.mdp);
+            this.Controls.Add(this.login);
+            this.Controls.Add(this.prenom);
+            this.Controls.Add(this.nom);
+            this.Name = "Form1";
+            this.Text = "  ";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox nom;
+        private System.Windows.Forms.TextBox prenom;
+        private System.Windows.Forms.TextBox login;
+        private System.Windows.Forms.TextBox mdp;
+        private System.Windows.Forms.TextBox log2;
+        private System.Windows.Forms.TextBox mdp2;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- Maquette/Maquette/Form1.cs
+++ Maquette/Maquette/Form1.cs
@@ -12,9 +12,41 @@ namespace Maquette
 {
     public partial class Form1 : Form
     {
+        MusiqueEntities musique;
+
         public Form1()
         {
             InitializeComponent();
+
+            musique = new MusiqueEntities();
+            Abonne_Load();
+        }
+
+        private void Abonne_Load()
+        {
+            var abonnes = (from a in musique.ABONNÉS
+                           select a).ToList();
+
+            foreach (ABONNÉS a in abonnes)
+            {
+                listBox1.Items.Add(a);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (nom.Text != "" && prenom.Text != "" && login.Text != "" && mdp.Text != "")
+            {
+                ABONNÉS a = new ABONNÉS();
+                a.PRÉNOM_ABONNÉ = prenom.Text;
+                a.NOM_ABONNÉ = nom.Text;
+                a.LOGIN_ABONNÉ = login.Text;
+                a.PASSWORD_ABONNÉ = mdp.Text;
+
+                listBox1.Items.Add(a);
+                musique.ABONNÉS.Add(a);
+                musique.SaveChanges();
+            }
         }
     }
 }
