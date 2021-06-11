d54130f8c142e06e08992f0d46424334f332f6b1
Léo PUYASTIER <leo.puyastier@etu.u-bordeaux.fr>
2021-06-11 09:22:12 +0200

    Correction de bugs + refactor de Abonné et Magasin

--- Maquette/Maquette/Magasin.cs
+++ Maquette/Maquette/Magasin.cs
@@ -13,19 +13,20 @@ namespace Maquette
 {
     public partial class Magasin : Form
     {
-        Abonné parent;
+
         List<ALBUMS> affichables;
         int nombreDePages;
-
         int pageDispo = 0;
+
+        readonly Abonné parent;
 
         public Magasin(Abonné parent)
         {
             InitializeComponent();
             this.parent = parent;
-            affichables = new List<ALBUMS>();            
-            affichables.AddRange(parent.disponibles);      
-            afficherAlbums();
+            affichables = new List<ALBUMS>();
+            affichables.AddRange(parent.disponibles);
+            AfficherAlbums();
             actualiserNombreDepages();
         }
 
@@ -34,37 +35,63 @@ namespace Maquette
             if (affichables.Count % 8 != 0)
             {
                 nombreDePages = affichables.Count / 8;
-            } else
+            }
+            else
             {
-                nombreDePages = (affichables.Count/8)-1;
+                nombreDePages = (affichables.Count / 8) - 1;
             }
         }
 
         #region IHM
 
-        private void lblEspace_Click(object sender, EventArgs e)
+        /// <summary>
+        /// Clic sur le label "Mon Espace"
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void LblEspace_Click(object sender, EventArgs e)
         {
-            Quitter();
+            AfficherEspace();
         }
 
-        private void btnRecherche_Click(object sender, EventArgs e)
+        /// <summary>
+        /// Clic sur le bouton de recherche
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void BtnRecherche_Click(object sender, EventArgs e)
         {
             PreparerAffichage();
         }
 
-        private void btnPreDis_Click(object sender, EventArgs e)
+        /// <summary>
+        /// Clic sur le bouton Page Précédente
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void BtnPreDis_Click(object sender, EventArgs e)
         {
             Decrementer();
         }
-        private void btnSuiDis_Click(object sender, EventArgs e)
+
+        /// <summary>
+        /// Clic sur le bouton Page Suivante
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void BtnSuiDis_Click(object sender, EventArgs e)
         {
             Incrementer();
         }
 
         #endregion
+
         #region Logique
 
-        private void Quitter()
+        /// <summary>
+        /// Change l'affichage sur la fenêtre Mon Espace
+        /// </summary>
+        private void AfficherEspace()
         {
             parent.ShowInTaskbar = true;
             parent.WindowState = FormWindowState.Normal;
@@ -73,42 +100,10 @@ namespace Maquette
             this.ShowInTaskbar = false;
         }
 
-        private void Decrementer()
-        {
-            if (pageDispo > 0)
-            {
-                pageDispo--;
-            }
-            else
-            {
-                pageDispo = nombreDePages;
-            }
-            afficherAlbums();
-        }
-
-        
-
-        private void Incrementer()
-        {
-            if (pageDispo < nombreDePages)
-            {
-                pageDispo++;
-            }
-            else
-            {
-                pageDispo = 0;
-            }
-            afficherAlbums();
-        }
-
-        #endregion
-
-
-        /// <summary>        
         /// Méthode ci-dessous : US 1
         /// Méthode pour afficher les albums non-empruntés
         /// </summary>
-        private void afficherAlbums()
+        private void AfficherAlbums()
         {
             panelDispo.Controls.Clear();
             for (int i = 8 * pageDispo; i < 8 * (pageDispo + 1); i++)
@@ -121,21 +116,57 @@ namespace Maquette
             lblPageDispo.Text = pageDispo + 1 + "";
         }
 
-        
+        /// <summary>
+        /// Décrémente la page des albums disponibles
+        /// </summary>
+        private void Decrementer()
+        {
+            if (pageDispo > 0)
+            {
+                pageDispo--;
+            }
+            else
+            {
+                pageDispo = nombreDePages;
+            }
+            AfficherAlbums();
+        }
 
+        /// <summary>
+        /// Décrémente la page des albums disponibles
+        /// </summary>
+        private void Incrementer()
+        {
+            if (pageDispo < nombreDePages)
+            {
+                pageDispo++;
+            }
+            else
+            {
+                pageDispo = 0;
+            }
+            AfficherAlbums();
+        }
+
+        /// <summary>
+        /// Permet de gérer l'emprunt d'un album
+        /// </summary>
+        /// <param name="album"></param>
         public void EmprunterAlbum(ALBUMS album)
         {
-            parent.EmprunterAlbum(album);          
+            parent.EmprunterAlbum(album);
             PreparerAffichage();
         }
 
-        
 
-        private void PreparerAffichage()
+        /// <summary>
+        /// Change l'affichage lors de la recherche d'un album
+        /// </summary>
+        public void PreparerAffichage()
         {
             if (barreRecherche.Text.Trim().Length != 0)
             {
-                affichables = parent.disponibles.FindAll(a => a.TITRE_ALBUM.StartsWith(barreRecherche.Text));               
+                affichables = parent.disponibles.FindAll(a => a.TITRE_ALBUM.StartsWith(barreRecherche.Text));
             }
             else
             {
@@ -143,8 +174,9 @@ namespace Maquette
             }
             pageDispo = 0;
             actualiserNombreDepages();
-            afficherAlbums();
-        }        
+            AfficherAlbums();
+        }
 
+        #endregion
     }
 }
